b50aca4617c8f2b1eb745ec4f23a3849489a502e
Fredrik Wiencke <fredrik@wiencke.net>
2015-11-11 15:10:28 +0100

    Initial commit

---
+++ GroupFilter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\work\SPV\TAX14_ODINMERGE.grp");
+            string[] linesToDelete = System.IO.File.ReadAllLines(@"C:\work\SPV\LinesToREmove.txt");
+
+            List<String> linesToWrite = new List<String>();
+
+            foreach (String line in lines)
+            {
+                if(line.StartsWith(","))
+                {
+                    continue;
+                }
+                
+                Boolean remove = false;
+                String testString = line.Replace(",", "");
+
+                foreach (String removeLine in linesToDelete)
+                { 
+                    if(removeLine.Equals(testString))
+                    {
+                        remove = true;
+                        break;
+                    }
+                }
+                if(!remove)
+                    linesToWrite.Add(line + ",");
+            }
+
+            System.IO.File.WriteAllLines(@"C:\work\SPV\output.txt", linesToWrite);
+        }
+    }
+}
